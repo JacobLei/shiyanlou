fec45980539d26a3dbcb0fc1a41db635ef68a424
JacobLei <17763636521@163.com>
2018-05-28 17:16:24 +0800

    change app.py

--- challenge/challenge_8/news/app.py
+++ challenge/challenge_8/news/app.py
@@ -4,11 +4,15 @@ import os
 import json
 from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/test'
 db = SQLAlchemy(app)
+conn = MongoClient('localhost', 27017)
+db_mongo = conn.tags
+tags_set = db.mongo.tags_set
 
 class File(db.Model):
     __tablename__ = 'file'
@@ -25,6 +29,17 @@ class File(db.Model):
         self.created_time = created_time
         self.category = category
         self.content = content
+
+    def add_tag(self, tag_name):
+        if tag_name not in tags_set.find(self.id):
+            tags_set.insert({self.id : tag_name})
+    
+    def remove_tag(self, tag_name):
+        tags_set.remove({self.id : tag_name})
+
+    @property
+    def tags(self):
+        return tags_set.find({self.id})
 
     def __repr__(self):
         return '<Course(title={})>'.format(self.title)
