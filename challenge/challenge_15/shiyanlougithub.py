9b25a0d86b14aa3546a6ab66f5dc85696d85e671
JacobLei <17763636521@163.com>
2018-06-08 20:43:54 +0800

    添加挑战15

---
+++ challenge/challenge_15/shiyanlougithub.py
@@ -0,0 +1,18 @@
+#-*- coding:utf-8 -*-
+
+import scrapy
+
+class ShiyanlouGithubSpider(scrapy.Spider):
+    name = 'shiyanlou-github'
+
+    @property
+    def start_urls(self):
+        url_tmp1 = 'https://github.com/shiyanlou?page={}&tab=repositories'
+        return (url_tmp1.format(i) for i in range(1, 5))
+
+    def parse(self, response):
+        for respository in response.css('li.col-12'):
+            yield {
+                    'name' : respository.xpath('.//div[contains(@class,"d-inline-block")]/h3/a/text()').re_first('[A-Za-z0-9]+'), 
+                    'update_time' : respository.xpath('.//relative-time/@datetime').extract_first()
+                    }
